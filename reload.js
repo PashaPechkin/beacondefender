1ddabe141cbdc7579149139f82402b73f57622e1
PashaPechkin <p.gurylev93@yandex.ru>
2017-10-12 17:31:55 +0300

    reload script
    add reload script

---
+++ reload.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         VkRefresh
+// @namespace    http://tampermonkey.net/
+// @version      0.131
+// @description  try to take over the world!
+// @author       You
+// @match        https://vk.com/app5990572_-22751485
+// @downloadURL  https://drive.google.com/uc?export=download&confirm=no_antivirus&id=0B3tijp3qx-9NQVJFNDZYVzZqRms
+// @updateURL    https://drive.google.com/uc?export=download&confirm=no_antivirus&id=0B3tijp3qx-9NQVJFNDZYVzZqRms
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(function(){
+        location.reload();
+    }, 30000);
+})();
